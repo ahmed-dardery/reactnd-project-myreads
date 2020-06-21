32fc7db74d6f7a3eb94a14b188c0be76bc7ed5b9
Megadardery <Megadardery@yahoo.com>
2020-06-21 19:49:51 +0200

    Updates

---
+++ precache-manifest.5c7be312e47b7b78aa223acc59576a8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb14057e5d5a2bae765281f8fc01af8e",
+    "url": "/reactnd-project-myreads/index.html"
+  },
+  {
+    "revision": "22b743ac227a0753354d",
+    "url": "/reactnd-project-myreads/static/css/main.4b3aae8d.chunk.css"
+  },
+  {
+    "revision": "c874783e2dac27542614",
+    "url": "/reactnd-project-myreads/static/js/2.675e7898.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactnd-project-myreads/static/js/2.675e7898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b743ac227a0753354d",
+    "url": "/reactnd-project-myreads/static/js/main.4bf0c330.chunk.js"
+  },
+  {
+    "revision": "a4b230cc8f2a62eae486",
+    "url": "/reactnd-project-myreads/static/js/runtime-main.e15fad77.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
